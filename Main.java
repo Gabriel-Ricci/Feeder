230f5c1c4d36c5236526f4e5909fbbffb9148d94
Gabriel-Ricci <gabrielr244@nycstudents.net>
2024-09-25 13:24:30 +0000

    Final

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 public class Main 
 {
-    public static void main(String[] args) 
+       public static void main(String[] args) 
     {
-        Feeder j = new Feeder(c:250);
+        Feeder i = new Feeder(500);
+        i.simulateOneDay(12);
+        System.out.println(i);
+        Feeder a = new Feeder(1000);
+        a.simulateOneDay(22);
+        System.out.println(a);
+        Feeder n = new Feeder(100);
+        n.simulateOneDay(5);
+        System.out.println(n);
+        Feeder j = new Feeder(2400);
+        System.out.println(j.simulateManyDays(10, 4));
+        Feeder p = new Feeder(250);
         System.out.println(j.simulateManyDays(10, 5));
-
+        Feeder l = new Feeder(0);
+        System.out.println(j.simulateManyDays(5, 10));
     }
 }
